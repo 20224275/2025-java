7341da2a982dbaae90ec2a002da3131790e5d7f4
20224275 <sparking1541@naver.com>
2025-09-11 16:27:45 +0900

    0911 수업실습

---
+++ intTestjava.java
@@ -0,0 +1,27 @@
+package sec03.exam2;
+
+import java.util.Scanner;
+
+public class intTestjava { 
+	
+	public static void mail(String[] args) {
+		byte val1 = 1;
+		byte val2 = 2;
+		
+		//byte val3 = val1 + val2;
+		
+//		System.out.printin("1/2:" +1/2);
+//		System.out.print("1.0/2.0:"+1.0/2.0);
+		String str = "10";
+		int value = 20;
+		//String str1 = str + vale;
+		String str1 = "" +value;
+		//int result = val1+ Interger.parseInt(str);
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.println(str1);
+		
+	}
+
+}
