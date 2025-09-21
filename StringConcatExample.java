c9f5e0143d8578f1b4ff0bb87d8f366390339921
20224275 <sparking1541@naver.com>
2025-09-21 18:06:20 +0900

    ch03 개인 실습

--- StringConcatExample.java
+++ StringConcatExample.java
@@ -1,22 +1,14 @@
-package sec03.exam06;
+package sec02.exam06;
 
 public class StringConcatExample {
 	public static void main(String[] args) {
+		String str1 = "JDK" + 6.0;
+		String str2 = str1 + "특징";
+		System.out.println(str2);
 		
-		int value = 10 + 2 + 8;
-		System.out.println("value: " + value);
-		
-		String str1 = 10 + 2 + "8";
-		System.out.println("str1: " + str1);
-		
-		String str2 = 10 + "2" + 8;
-		System.out.println("str2: " + str2);
-		
-		String str3 = "10" + 2 + 8;
-		System.out.println("str3: " + str3);
-		
-		String str4 = "10" + (2 + 8);
-		System.out.println("str4: " + str4);
+		String str3 = "JDK" + 3 + 3.0;
+		String str4 = 3 + 3.0 + "JDK";
+		System.out.println(str3);
+		System.out.println(str4);		
 	}
 }
-
